e9f4d727a20915f6a6ec36744e5527dc037562e8
CanKeles5 <cankeles22@gmail.com>
2022-02-10 22:46:31 +0300

    Refactor

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import utils
 import metrics
 import models
@@ -11,17 +10,4 @@ import numpy as np
 if __name__ == '__main__':
     utils.build_whamr_like_dataset(speakers_path="D:\SpeechSeperationData\chunks", wham_noise_path="D:\whamr\wham_noise\wham_noise", output_root="D:\SpeechSeperationData")
     
-=======
-import utils
-import metrics
-import models
-from scipy.io import wavfile
-from scipy import signal
-from scipy.io.wavfile import write #remove this and use wavfile.write
-import numpy as np
 
-
-if __name__ == '__main__':
-    utils.build_whamr_like_dataset(speakers_path="D:\SpeechSeperationData\chunks", wham_noise_path="D:\whamr\wham_noise\wham_noise", output_root="D:\SpeechSeperationData")
-    
->>>>>>> 5d8c10f3f90e687c07f31772b03fc74ce26177f1

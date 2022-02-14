c1ef229ef26369e09a20b3e8dee32ca4e2cbb76d
CanKeles5 <cankeles22@gmail.com>
2022-02-14 12:39:32 +0300

    Fine tune option for ConvTasNet

--- train_models.py
+++ train_models.py
@@ -2,21 +2,37 @@
 Script to train models from different frameworks
 
 To-Do:
-- Train SepFormer (SpeechBrain)
-- Train custom model (SpeechBrain)
 - Train ConvTasNet (Asteroid)
+    - Add option to load model & fine tune
+
+- Train DPTNet (Asteroid)
+
 - Train Demucs (Demucs repo)
+
+- Train SSE (self-supervised senh)
+    - Refactor code to newer Pytorch versions
+    - Add option to only train supervised (not priority right now)
+    - Try to see if different model arch. can be used for the same script
+
+----------------------This is not priority
+- Train SepFormer (SpeechBrain) -> The code seems broken
+
+- Train custom model (SpeechBrain)
 
 - For all models, have the same API
 '''
 
-def train_model(model_name):
+
+def train_model(model_name: str):
     model_name = model_name.lower()
     
-    if model_name == "sepformer":
-        pass
-    elif model_name == "convtasnet":
-        #we might call the training script here, the config would be the tricky part
+    if model_name == "convtasnet":
+        import training_scripts.convtasnet.train as train_convtasnet
+        train_convtasnet.main()
+    if model_name == "dptnet":
+        import training_scripts.dptnet.train as train_dptnet
+        train_dptnet.main()
+    elif model_name == "sepformer":
         pass
     elif model_name == "demucs":
         pass
